c72794bd8f3f6a8a9350d17ff4ff4c70ecd88a8c
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-08-30 23:48:20 -0500

    feat(router): add routing for Signup, Login, Profile, Home, Portfolios, and Reports pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./components/home/home.component";
+import {PortfoliosComponent} from "./components/portfolios/portfolios.component";
+import {ReportsComponent} from "./components/reports/reports.component";
+import {LoginComponent} from "./components/users/login/login.component";
+import {SignupComponent} from "./components/users/signup/signup.component";
+import {NotfoundComponent} from "./components/notfound/notfound.component";
+import {ProfileComponent} from "./components/profile/profile.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'portfolios', component: PortfoliosComponent },
+  { path: 'reports', component: ReportsComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'profile', component: ProfileComponent},
+  { path: '', redirectTo: '/home', pathMatch: 'full' }, // Redirigir a 'home' por defecto
+  { path: '**', component: NotfoundComponent } // Wildcard route for 404 page
+];
+
+
+
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component';
+import { PortfoliosComponent } from './components/portfolios/portfolios.component';
+import { ReportsComponent } from './components/reports/reports.component';
+import { LoginComponent } from './components/users/login/login.component';
+import { SignupComponent } from './components/users/signup/signup.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProfileComponent } from './components/profile/profile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    HomeComponent,
+    PortfoliosComponent,
+    ReportsComponent,
+    LoginComponent,
+    SignupComponent,
+    NotfoundComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
