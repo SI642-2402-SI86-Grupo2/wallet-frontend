6283a033ed31d2075c18a0b19b74852d51e4ebe7
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-09-14 23:37:02 -0500

    feat(home): update home component

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -9,9 +9,15 @@ import { Application } from '@splinetool/runtime';
 export class HomeComponent implements AfterViewInit {
 
   ngAfterViewInit() {
-//<canvas id="canvas3d" class="w-full h-auto object-cover"></canvas>
-    const canvas = document.getElementById('canvas3d') as HTMLCanvasElement;
-    const app = new Application(canvas);
-    app.load('https://prod.spline.design/9bp-r62lcNeqZ7Jx/scene.splinecode');
+//<canvas id="canvas3d-1" class="w-full h-auto object-cover"></canvas>
+    // Initialize first Spline application
+    const canvas1 = document.getElementById('canvas3d-1') as HTMLCanvasElement;
+    const app1 = new Application(canvas1);
+    app1.load('https://prod.spline.design/9bp-r62lcNeqZ7Jx/scene.splinecode');
+
+    // Initialize second Spline application
+    const canvas2 = document.getElementById('canvas3d-2') as HTMLCanvasElement;
+    const app2 = new Application(canvas2);
+    app2.load('https://prod.spline.design/hczBqXhu-Hr7Vfpy/scene.splinecode');
   }
 }
