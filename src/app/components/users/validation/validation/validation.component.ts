0d5679e20df9fd7d850e2bf8b3b8e8e352a1ede4
Estefano-Lostaunau <u202211742@upc.edu.pe>
2024-10-24 18:23:54 -0500

    feat(validation): update validcodes

--- src/app/components/users/validation/validation/validation.component.ts
+++ src/app/components/users/validation/validation/validation.component.ts
@@ -7,7 +7,7 @@ import { Router } from '@angular/router';
   styleUrl: './validation.component.css'
 })
 export class ValidationComponent {
-  validCodes = ['u20221a928', 'U20221a928','u20211c807','U20211c807','u202218451','U202218451','u202211742','U202211742']; // Códigos válidos
+  validCodes = ['u20221A928', 'U20221A928','u20221a928', 'U20221a928','u20211C807','U20211C807','u20211c807','U20211c807','u202218451','U202218451','u202211742','U202211742','u202120467','U202120467']; // Códigos válidos
   inputCode: string = ''; // Para almacenar el código ingresado
   message: string = ''; // Mensaje de resultado
 
